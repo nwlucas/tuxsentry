98f35e326c6d9706e071b575a54f75f846e2e0ff
Nigel Williams-Lucas <yesniah@gmail.com>
2017-04-03 23:59:11 -0400

    Fixed Facts.Disk.partitions to return only physical disks when requested. Default behaviour is to return all mounted partitions

--- apps/facts/lib/facts/disk/disk.ex
+++ apps/facts/lib/facts/disk/disk.ex
@@ -11,22 +11,18 @@ defmodule Facts.Disk do
   with the info. Depending on the value of all it will return all disks if true, or only physical disks if false.
   """
   @spec partitions(boolean) :: list(%Facts.Disk.PartitionStat{})
-  def partitions(all \\ false) do
+  def partitions(all \\ true) do
 
     try do
-      mtab = read_mtab()
-      fs = get_file_systems()
+      p = read_mtab()
+        |> get_file_systems(all)
+        |> generate_list()
 
-      case all do
-        true -> fs = filter_file_systems(fs, mtab)
-      end
-
-      {:ok, {mtab, fs}}
+      {:ok, p}
     rescue
       e -> Logger.error "Error occured while attempting to gather partitions facts: " <> e
       {:error, e}
     end
-
   end
 
   @spec read_mtab :: list(list(String.t))
@@ -40,22 +36,28 @@ defmodule Facts.Disk do
     lines
   end
 
-  @spec generate_list(data :: list, file_systems :: list, options :: list) :: list(%Facts.Disk.PartitionStat{})
-  defp generate_list(raw, fs, opts) do
-
-  end
-
-  @spec get_file_systems :: list(String.t)
-  defp get_file_systems() do
+  @spec get_file_systems(mtab :: list, all :: boolean) :: list(String.t)
+  defp get_file_systems(mtab, all) do
     filename = host_proc("filesystems")
-    lines = read_file(filename)
+    fs = read_file(filename)
             |> String.split("\n")
             |> Enum.filter(& !(String.length(&1) == 0))
-            |> Enum.map(& String.trim_leading(&1, "nodev"))
+            |> Enum.reject(& String.starts_with?(&1, "nodev"))
             |> Enum.map(& String.trim_leading(&1, "\t"))
-    lines
+
+    data =
+      case all do
+        false -> Enum.filter(mtab, & Enum.member?(fs, Enum.fetch!(&1, 2)))
+        _ -> mtab
+      end
+
+    data
   end
 
+  @spec generate_list(data :: list) :: list(%Facts.Disk.PartitionStat{})
+  defp generate_list(data) do
+    data
+  end
 #  @spec get_fs_type :: listy
 #  defp get_fs_type() do
 #
