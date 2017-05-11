48ae9de78352c3ab2755b65b96db7da127332b0c
Nigel Williams-Lucas <yesniah@gmail.com>
2017-05-11 18:47:02 -0400

    Moving Facts to its own library.

--- apps/facts/lib/facts/net/net.ex
+++
@@ -1,6 +0,0 @@
-defmodule Facts.Net do
-  @moduledoc """
-  """
-
-
-end
--- apps/facts/test/info_stat_test.exs
+++
@@ -1,6 +0,0 @@
-defmodule Facts.CPU.InfoStatTest do
-  alias Facts.CPU.InfoStat
-  use ExUnit.Case, async: true
-  doctest InfoStat
-
-end
